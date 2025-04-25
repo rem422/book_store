bb53ca737c779abf0fb09e5097c9568bbf287bfe
Rem Simiyu <remsimiyu@gmail.com>
2025-04-26 02:34:02 +0300

    Book Store

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,8 +1,85 @@
-import React from 'react'
+import React, { useState} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import BackButton from '../Components/BackButton';
+import Spinner from '../Components/Spinner';
 
 const CreateBook = () => {
+  const [cover, setCover] = useState('');
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSaveBook = () => {
+    const data = {
+      cover,
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .post('http://localhost:3000/books', data)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      }).catch((error) => {
+        setLoading(false);
+        alert('An error happened. Please check the console');
+        console.log(error);
+      });
+  };
+
   return (
-    <div>CreateBook</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Create Book</h1>
+
+      {loading ? <Spinner /> : ""}
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Cover</label>
+          <input 
+            type="text" 
+            value={cover}
+            onChange = {(e) => setCover(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
+          <input 
+            type="text" 
+            value={title}
+            onChange = {(e) => setTitle(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
+          <input 
+            type="text" 
+            value={author}
+            onChange = {(e) => setAuthor(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <input 
+            type="text" 
+            value={publishYear}
+            onChange = {(e) => setPublishYear(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <button className='p-2 bg-sky-300 m-8 w-full mx-auto' onClick={handleSaveBook}>
+          Save
+        </button>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,8 +1,103 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import BackButton from '../Components/BackButton';
+import Spinner from '../Components/Spinner';
 
 const EditBook = () => {
+  const [cover, setCover] = useState('');
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(`http://localhost:3000/books/${id}`)
+      .then((res) => {
+        setCover(res.data.cover);
+        setTitle(res.data.title);
+        setAuthor(res.data.author);
+        setPublishYear(res.data.publishYear);
+        setLoading(false);
+      }).catch((error) => {
+        setLoading(false);
+        alert('An error happened. Please check the console');
+        console.log(error);
+      });
+  }, []);
+
+    const handleEditBook = () => {
+    const data = {
+      cover,
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .put(`http://localhost:3000/books/${id}`, data)
+      .then(() => {
+        setLoading(false);
+        navigate('/');
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert('An error happened. Please check the console');
+        console.log(error);
+      });
+  };
+
   return (
-    <div>EditBook</div>
+    <div className='p-4'>
+      <BackButton />
+      <h1 className='text-3xl my-4'>Edit Book</h1>
+
+      {loading ? <Spinner /> : ""}
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Cover</label>
+          <input 
+            type="text" 
+            value={cover}
+            onChange = {(e) => setCover(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
+          <input 
+            type="text" 
+            value={title}
+            onChange = {(e) => setTitle(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
+          <input 
+            type="text" 
+            value={author}
+            onChange = {(e) => setAuthor(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+          <input 
+            type="text" 
+            value={publishYear}
+            onChange = {(e) => setPublishYear(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+        <button className='p-2 bg-sky-300 m-8 w-full mx-auto' onClick={handleEditBook}>
+          Save
+        </button>
+      </div>
+    </div>
   )
 }
 
